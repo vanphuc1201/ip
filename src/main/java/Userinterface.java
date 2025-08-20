2f0f56eae5ceb1e9ace470b26a9e7cf1f7db0a0f
vanphuc <e1375522@u.nus.edu>
2025-08-21 00:26:55 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Userinterface.java
@@ -0,0 +1,16 @@
+public class Userinterface {
+    private final String line = "____________________________________________________________";
+
+    public void sayHello() {
+        System.out.println(line);
+        System.out.println(" Hello! I'm Phuc");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+    }
+
+    public void sayGoodbye() {
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
