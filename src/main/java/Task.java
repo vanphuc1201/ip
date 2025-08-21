fd3c5dce3307e177405725c7a61d7d56643b5b41
vanphuc <e1375522@u.nus.edu>
2025-08-21 18:40:57 +0800

    Describe a task and some method in it

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public void setNotDone() {
+        this.isDone = false;
+    }
+
+    public String printtask() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
