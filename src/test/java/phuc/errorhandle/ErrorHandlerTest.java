728d853ff2a645134846f7cfcf4984e90cc666f7
vanphuc <e1375522@u.nus.edu>
2025-08-30 01:50:29 +0800

    Add Junit tests for ErrorHandler class
    Add ErrorHandlerTest class to test many methods:
    * validateTaskIndex so that it can check if the index is suitable or not
    * validateDescription so that it can check if the description is correct format or not

---
+++ src/test/java/phuc/errorhandle/ErrorHandlerTest.java
@@ -0,0 +1,49 @@
+package phuc.errorhandle;
+
+import org.junit.jupiter.api.Test;
+import phuc.exception.PhucException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ErrorHandlerTest {
+
+    @Test
+    public void testValidateTaskIndex_valid() {
+        assertDoesNotThrow(() -> {
+            ErrorHandler.validateTaskIndex("1", 5);
+        });
+    }
+
+    @Test
+    public void testValidateTaskIndex_invalid() {
+        assertThrows(PhucException.class, () -> {
+            ErrorHandler.validateTaskIndex("0", 5);
+        });
+
+        assertThrows(PhucException.class, () -> {
+            ErrorHandler.validateTaskIndex("6", 5);
+        });
+
+        assertThrows(PhucException.class, () -> {
+            ErrorHandler.validateTaskIndex("abc", 5);
+        });
+    }
+
+    @Test
+    public void testValidateDescription_valid() {
+        assertDoesNotThrow(() -> {
+            ErrorHandler.validateDescription("valid description", "todo", "todo");
+        });
+    }
+
+    @Test
+    public void testValidateDescription_empty() {
+        assertThrows(PhucException.class, () -> {
+            ErrorHandler.validateDescription("", "todo", "todo");
+        });
+
+        assertThrows(PhucException.class, () -> {
+            ErrorHandler.validateDescription("   ", "todo", "todo");
+        });
+    }
+}
